1e15cf81586b210ccc811645e7def9f084d6ab95
Mykhailyk-Vadym <ihappyspam@gmail.com>
2023-05-03 14:56:05 +0300

    testing  message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Вася учи Айтиху.
         </p>
         <a
           className="App-link"
